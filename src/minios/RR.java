cb055b605003488200c8bec048ea81cfbadd22ab
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2026-09-10 20:48:09 -0400

    Basic Setup

---
+++ src/minios/RR.java
@@ -0,0 +1,15 @@
+package minios;
+
+import java.util.List;
+
+public class RR implements SchedulingAlgo{
+    @Override
+    public void addProcess(List<Process> readyQueue, Process p) {
+
+    }
+
+    @Override
+    public Process selectNextProcess(List<Process> readyQueue) {
+        return null;
+    }
+}
